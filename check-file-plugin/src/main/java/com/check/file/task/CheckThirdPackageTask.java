c4f1ce01d0f6e6e73c1a2855172fdfd6e9fa408b
王江 <wangjiang@wangjiangdeMacBook-Pro.local>
2019-11-30 16:14:42 +0800

    Fix. 去掉系统库依赖

--- check-file-plugin/src/main/java/com/check/file/task/CheckThirdPackageTask.java
+++ check-file-plugin/src/main/java/com/check/file/task/CheckThirdPackageTask.java
@@ -1,41 +1,20 @@
 package com.check.file.task;
 
 import com.check.file.CheckFileExtension;
-import com.check.file.task.BaseDefaultTask;
-import com.check.file.util.FileUtil;
-import com.check.file.util.LoggerUtil;
 
-import org.gradle.BuildAdapter;
-import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.ClientModule;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.artifacts.ExternalDependency;
-import org.gradle.api.artifacts.ExternalModuleDependency;
 import org.gradle.api.artifacts.FileCollectionDependency;
-import org.gradle.api.artifacts.ModuleDependency;
-import org.gradle.api.artifacts.ModuleVersionSelector;
 import org.gradle.api.artifacts.ProjectDependency;
-import org.gradle.api.artifacts.SelfResolvingDependency;
-import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency;
-import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency;
-import org.gradle.api.invocation.Gradle;
-import org.gradle.api.tasks.TaskAction;
-import org.gradle.internal.impldep.it.unimi.dsi.fastutil.Hash;
-import org.slf4j.Logger;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Set;
-
-import groovy.lang.Closure;
 
 public class CheckThirdPackageTask extends BaseDefaultTask {
 
@@ -74,7 +53,9 @@ public class CheckThirdPackageTask extends BaseDefaultTask {
                             projects.add(dependencyProject);
                         }
                     } else if (dependency instanceof FileCollectionDependency) {
-                        files.addAll(((FileCollectionDependency) dependency).getFiles().getFiles());
+                        if (dependency.getGroup() != null) {
+                            files.addAll(((FileCollectionDependency) dependency).getFiles().getFiles());
+                        }
                     }
                 }
             }
